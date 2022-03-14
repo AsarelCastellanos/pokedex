5271dcf9ebb93d921f28b5261af20b0cb73dac7b
Asarel Castellanos <asarelc@gmail.com>
2022-03-13 21:56:10 -0700

    Changed layout so now pokemon can be displayed by generation.
    I am stuck on how to display images of each of the pokemon.

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,4 +1,32 @@
+<script>
+    import { fetchPokemon, pokemon } from "../stores/pokemart";
+
+    function loadFirstGen() {
+        fetchPokemon(151, 0);
+    }
+
+    function loadSecondGen() {
+        fetchPokemon(100, 151);
+    }
+
+    function loadedThirdGen() {
+        fetchPokemon(135, 251);
+    }
+
+    function loadFourthGen() {
+        fetchPokemon(107, 386)
+    }
+
+    function loadFifthGen() {
+        fetchPokemon(156, 493)
+    }
+</script>
+
 <nav class="flex justify-center w-full">
-    <a class="mx-4 text-lg" href="/">Home</a>
+    <button on:click={loadFirstGen} class="mx-4 text-lg">Gen 1</button>
+    <button on:click={loadSecondGen} class="mx-4 text-lg">Gen 2</button>
+    <button on:click={loadedThirdGen} class="mx-4 text-lg">Gen 3</button>
+    <button on:click={loadFourthGen} class="mx-4 text-lg">Gen 4</button>
+    <button on:click={loadFifthGen} class="mx-4 text-lg">Gen 5</button>
     <a class="mx-4 text-lg" href="/about">About</a>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
-<script context="module">
+<!-- <script context="module">
     export async function load({fetch}) {
-        const url = 'https://pokeapi.co/api/v2/pokemon?limit=150';
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
         const res = await fetch(url);
         const data = await res.json();
         const loadedPokemon = data.results.map((data, index) => {
@@ -14,30 +14,34 @@
         });
         return {props: {pokemon:loadedPokemon}}
     }
-</script>
+</script> -->
 <script>
-    export let pokemon;
+    import { pokemon } from "../stores/pokemart";
     import PokemonCard from "../components/pokemonCard.svelte";
 
-    let searchTerm = "";
-    let filteredPokemon = [];
+    // let searchTerm = "";
+    // let filteredPokemon = [];
 
-    $: {
-        console.log(searchTerm);
-        if (searchTerm) {
-            filteredPokemon = pokemon.filter(poke => poke.name.toLowerCase().includes(searchTerm.toLowerCase()));
-        } else {
-            filteredPokemon = [...pokemon];
-        }
-    }
+    // $: {
+    //     console.log(searchTerm);
+    //     if (searchTerm) {
+    //         filteredPokemon = pokemon.filter(poke => poke.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    //     } else {
+    //         filteredPokemon = [...pokemon];
+    //     }
+    // }
 </script>
 
 <h1 class="text-4xl text-center my-8 uppercase">Pokedex</h1>
 
-<input class="w-full rounded-md text-lg p-4 border-2 border-grey-200" bind:value={searchTerm} type="text" placeholder="Search Pokemon">
+<!-- <input class="w-full rounded-md text-lg p-4 border-2 border-grey-200" bind:value={searchTerm} type="text" placeholder="Search Pokemon"> -->
 
-<div class="py-4 grid gap-4 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+<!-- <div class="py-4 grid gap-4 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
     {#each filteredPokemon as poke}
         <PokemonCard poke={poke}/>
     {/each}
-</div>
+</div> -->
+
+{#each $pokemon as poke}
+        <PokemonCard poke={poke}/>
+{/each}
